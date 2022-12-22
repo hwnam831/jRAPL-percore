d37670dbd4c280a693d2b4cee5f021088efcf88c
Hyoungwook Nam <hwnam831@gmail.com>
2022-12-22 01:13:38 -0600

    exp script update

--- expscript.sh
+++ expscript.sh
@@ -1,1 +1,1 @@
-sudo java TraceCollector 150 > $1_$2.csv & ssh hwnam831@ow1 "cd /mydata/workspace/faas-profiler; ./WorkloadInvoker -c "$1".json"
+sudo java TraceCollector 180 > $1_$2.csv & ssh hwnam831@ow1 "cd /mydata/workspace/faas-profiler; ./WorkloadInvoker -c "$1".json"
--- wrapper.sh
+++ wrapper.sh
@@ -1,1 +1,2 @@
 ./set_freq.sh $1; ./expscript.sh myconfig $1; ./expscript.sh autocomplete $1; ./expscript.sh ocr-img $1; ./expscript.sh img-resize $1; ./expscript.sh markdown $1; ./expscript.sh sentiment $1;
+#./turbowrapper.sh; ./disable_turbo.sh; ./wrapper.sh 1200;./wrapper.sh 1300;./wrapper.sh 1400;./wrapper.sh 1500;./wrapper.sh 1600;./wrapper.sh 1700;./wrapper.sh 1800;./wrapper.sh 1900;./wrapper.sh 2000;./wrapper.sh 2100;./wrapper.sh 2200;./wrapper.sh 2300;./wrapper.sh 2400;./wrapper.sh 2500;./wrapper.sh 2600;
