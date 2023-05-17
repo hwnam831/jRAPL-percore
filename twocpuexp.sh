164688b15391bc577431121f9ae9ac54446b5f25
Hyoungwook Nam <hwnam831@gmail.com>
2023-05-17 15:58:32 -0500

    Collect full trace instead of snapshot

--- twocpuexp.sh
+++ twocpuexp.sh
@@ -1,4 +1,4 @@
 #twocpuexp.sh [policy] [app1] [app2] [cap]
-ssh hwnam831@ow1 "bash /local/repository/resetow.sh; sleep 150; bash /local/repository/install_functions.sh";
-ssh hwnam831@ow1 "cd /mydata/workspace/faas-profiler; ./WorkloadInvoker -c "$2"_"$3".json" & sleep 10;
-sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --policy $1 --duration 50 --cap $4 > $1_$2_$3.csv;
+ssh hwnam831@ow1 "bash /local/repository/resetow.sh; sleep 180; bash /local/repository/install_functions.sh";
+sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --policy $1 --duration 120 --cap $4 > $1_$2_$3.csv &
+ssh hwnam831@ow1 "cd /mydata/workspace/faas-profiler; ./WorkloadInvoker -c "$2"_"$3".json"
