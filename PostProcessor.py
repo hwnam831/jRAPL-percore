e5e277bd0335a0f3ae7b94aae3e8e4ff0eac2660
Hyoungwook Nam <hwnam831@gmail.com>
2023-05-24 13:08:56 -0500

    csv postprocessor

---
+++ PostProcessor.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+sheetnames = []
+for fname in os.listdir():
+    if fname[-4:] == '.csv':
+        sheetnames.append(fname[:-4])
+def postprocess(csvfile):
+    df = pd.read_csv(csvfile)
+    df = df.rename(columns={df.columns[1]:'power:0',df.columns[2]:'power:1',df.columns[4]:'freq:0',df.columns[5]:'freq:1',\
+                        df.columns[7]:'powerpredict:0',df.columns[8]:'powerpredict:1',df.columns[16]:'bipspredict:0',df.columns[17]:'bipspredict:1',\
+                  df.columns[10]:'grad:0',df.columns[11]:'grad:1', df.columns[13]:'bips:0',df.columns[14]:'bips:1',\
+                  df.columns[19]:'powerlimit:0', df.columns[20]:'powerlimit:1',df.columns[22]:'newlimit:0', df.columns[23]:'newlimit:1'})
+    df['B2/W:0'] = df['bips:0']*df['bips:0']/df['power:0']
+    df['B2/W:1'] = df['bips:1']*df['bips:1']/df['power:1']
+    for i in range(len(df)-100,0,-1):
+        if df['B2/W:0'][i] > 2.0 or df['B2/W:1'][i] > 2.0:
+            print(i)
+            df = df.iloc[:i+1]
+            break
+    return df.drop(columns=['Cur power usage', 'Freq', 'Prediction', 'Gradients', 'Cur perf', 'Bips Prediction', 'New power limit', 'Cur power limit'])
+    import re
+
+xlsxnames = {}
+
+for name in sheetnames:
+    df = postprocess(name + '.csv')
+    m = re.match(r"([a-zA-Z0-9]+)\_(.+)\_(\d+)",name)
+    if m is None:
+        print(name)
+        continue
+    wmode = 'w'
+    if os.path.isfile(m.group(1) + '_' + m.group(3) + '.xlsx'):
+        wmode = 'a'
+    with pd.ExcelWriter(m.group(1) + '_' + m.group(3) + '.xlsx', mode=wmode) as writer:
+        df.to_excel(writer, sheet_name=m.group(2))
