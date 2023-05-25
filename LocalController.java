dff956e281951bb46b92403bb496be03434ea8cf
Hyoungwook Nam <hwnam831@gmail.com>
2023-05-24 22:36:43 -0500

    Change RAPL period

--- LocalController.java
+++ LocalController.java
@@ -249,7 +249,7 @@ class PowerControllerThread extends Thread{
 		    System.err.println("Power limit1 of pkg " + s + ": " + limitinfo[0] + "\t timewindow1 :" + limitinfo[1]);
 		    System.err.println("Power limit2 of pkg " + s + ": " + limitinfo[2] + "\t timewindow2 :" + limitinfo[3]);
             System.err.println("Trying to set running average timewindow to " + timeperiod/2 + "ms");
-            EnergyCheckUtils.SetRAPLTimeWindow(s, timeperiod/2);
+            EnergyCheckUtils.SetRAPLTimeWindow(s, timeperiod);
             System.err.println("Trying to set running average limit to " + curpl[s] + "W");
 			EnergyCheckUtils.SetPkgLimit(s, curpl[s], curpl[s]+20);
         }
