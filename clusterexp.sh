b5a27309d3e3a9402bd4ae254b2a7ac3251c72f3
Hyoungwook Nam <hwnam831@gmail.com>
2023-10-10 15:24:28 -0500

    script for different policies

--- clusterexp.sh
+++ clusterexp.sh
@@ -1,6 +1,6 @@
 #Usage: clusterexp.sh [policy] [limit]
 PERNODECAP=$(($2/2))
-python3 ClusterController.py --policy $1 --limit $2 --duration 120 > clusterexp1log.csv&
-ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_hierarchical.sh ml1 90 "$PERNODECAP" clusterexp1" &
-ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_hierarchical.sh ml1 90 "$PERNODECAP" clusterexp1" &
+python3 ClusterController.py --policy $1 --limit $2 --duration 120 > clusterexp1_$1.csv&
+ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_hierarchical.sh "$1" 90 "$PERNODECAP" clusterexp1"$1"" &
+ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_hierarchical.sh "$1" 90 "$PERNODECAP" clusterexp1"$1"" &
 cd /mydata/workspace/faas-profiler; ./WorkloadInvoker -c clusterexp1.json
