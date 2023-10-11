f69822ea14d2772000ff9fffc623c205d040e62c
Hyoungwook Nam <hwnam831@gmail.com>
2023-10-10 20:23:25 -0500

    sleep more

--- clusterexp.sh
+++ clusterexp.sh
@@ -4,4 +4,4 @@ python3 ClusterController.py --policy $1 --limit $2 --duration 120 > clusterexp1
 ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_hierarchical.sh "$1" 90 "$PERNODECAP" clusterexp1"$1"" &
 ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_hierarchical.sh "$1" 90 "$PERNODECAP" clusterexp1"$1"" &
 cd /mydata/workspace/faas-profiler; ./WorkloadInvoker -c clusterexp1.json &
-sleep 120
+sleep 150
