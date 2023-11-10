93858560840346052f122671abe132b475174dde
Hyoungwook Nam <hwnam831@gmail.com>
2023-11-10 16:23:19 -0600

    localcontrol lr change

--- run_localcontrol.sh
+++ run_localcontrol.sh
@@ -1,2 +1,2 @@
 #Usage run_.sh [policy] [duration] [cap]
-sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --policy $1 --duration $2 --cap $3 > local_$1_$3.csv
+sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --policy $1 --duration $2 --cap $3 --lr 1.0 > local_$1_$3.csv
