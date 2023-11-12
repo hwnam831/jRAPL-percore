5b45f4d9c634e8f2b33a3d81f60a843c468ac7ab
Hyoungwook Nam <hwnam831@gmail.com>
2023-11-12 16:13:55 -0600

    warmup before control

--- localcontrolexp.sh
+++ localcontrolexp.sh
@@ -1,7 +1,9 @@
 #Usage: clusterexp.sh [policy] [pernodelimit]
+cd /mydata/workspace/faas-profiler;
+./WorkloadInvoker -c warmup.json;
+sleep 60;
 ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_localcontrol.sh "$1" 120 "$2"" &
 ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_localcontrol.sh "$1" 120 "$2"" &
 ssh hwnam831@ow4 "cd /mydata/workspace/jrapl; bash run_localcontrol.sh "$1" 120 "$2"" &
 ssh hwnam831@ow5 "cd /mydata/workspace/jrapl; bash run_localcontrol.sh "$1" 120 "$2"" &
-cd /mydata/workspace/faas-profiler; ./WorkloadInvoker -c clusterexp5.json &
-sleep 180
+./WorkloadInvoker -c clusterexp5.json & sleep 180
