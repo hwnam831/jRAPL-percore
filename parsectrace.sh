bed88f7779e75e257d5cbc2fc907182ab681fa02
Hyoungwook Nam <hwnam831@gmail.com>
2025-01-03 02:12:54 -0600

    wrapper and longer trace

--- parsectrace.sh
+++ parsectrace.sh
@@ -1,3 +1,3 @@
 bash set_freq.sh $1;
 taskset -c 0-9 parsecmgmt -a run -p $2 -i native -n 10 & taskset -c 10-19 parsecmgmt -a run -p $3 -i native -n 10 &
-sudo java TraceCollector 60 > $2_$3_$1.csv
+sudo java TraceCollector 90 > $2_$3_$1.csv
---
+++ parsecwrapper.sh
@@ -0,0 +1,8 @@
+bash parsectrace 2600 $1 $2; sleep 30;
+bash parsectrace 2400 $1 $2; sleep 30;
+bash parsectrace 2200 $1 $2; sleep 30;
+bash parsectrace 2000 $1 $2; sleep 30;
+bash parsectrace 1800 $1 $2; sleep 30;
+bash parsectrace 1600 $1 $2; sleep 30;
+bash parsectrace 1400 $1 $2; sleep 30;
+bash parsectrace 1200 $1 $2; sleep 30;
