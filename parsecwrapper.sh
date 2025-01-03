97387b9d85c87496009f77b6fac4264b9cfc217c
Hyoungwook Nam <hwnam831@gmail.com>
2025-01-03 02:14:55 -0600

    parsecwrapper type

--- parsecwrapper.sh
+++ parsecwrapper.sh
@@ -1,8 +1,8 @@
-bash parsectrace 2600 $1 $2; sleep 30;
-bash parsectrace 2400 $1 $2; sleep 30;
-bash parsectrace 2200 $1 $2; sleep 30;
-bash parsectrace 2000 $1 $2; sleep 30;
-bash parsectrace 1800 $1 $2; sleep 30;
-bash parsectrace 1600 $1 $2; sleep 30;
-bash parsectrace 1400 $1 $2; sleep 30;
-bash parsectrace 1200 $1 $2; sleep 30;
+bash parsectrace.sh 2600 $1 $2; sleep 30;
+bash parsectrace.sh 2400 $1 $2; sleep 30;
+bash parsectrace.sh 2200 $1 $2; sleep 30;
+bash parsectrace.sh 2000 $1 $2; sleep 30;
+bash parsectrace.sh 1800 $1 $2; sleep 30;
+bash parsectrace.sh 1600 $1 $2; sleep 30;
+bash parsectrace.sh 1400 $1 $2; sleep 30;
+bash parsectrace.sh 1200 $1 $2; sleep 30;
