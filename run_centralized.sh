e38ee43f8a88573e8d9482ad358d57048721486d
Hyoungwook Nam <hwnam831@gmail.com>
2025-03-13 15:40:12 -0500

    script bug fix 2

--- run_centralized.sh
+++ run_centralized.sh
@@ -2,6 +2,6 @@
 MYTAG="${5:-fair}"
 first="${6:-high}"
 second="${7:-low}"
-bash exec_container.sh $3 0 $first $1 $MYTAG_$2 &
-bash exec_container.sh $4 1 $second $1 $MYTAG_$2 &
+bash exec_container.sh $3 0 $first $1 $MYTAG"_"$2 &
+bash exec_container.sh $4 1 $second $1 $MYTAG"_"$2 &
 sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --parent 10.10.1.1 --policy central --duration $1 --cap $2 > $3_$4_central_$2.csv
