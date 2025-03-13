5bf786f11b7fa634f42d86c9dfd68d47e8368aaf
Hyoungwook Nam <hwnam831@gmail.com>
2025-03-13 15:25:26 -0500

    script fix

--- run_centralized.sh
+++ run_centralized.sh
@@ -1,7 +1,7 @@
-#Usage run_.sh [duration] [cap] [app1] [app2] [load1] [load2] [tag]
-first="${5:-high}"
-second="${6:-low}"
-MYTAG="${7:-fair}"
+#Usage run_.sh [duration] [cap] [app1] [app2] [tag] [load1] [load2]
+MYTAG="${5:-fair}"
+first="${6:-high}"
+second="${7:-low}"
 bash exec_container.sh $3 0 $first $1 $MYTAG_$2 &
 bash exec_container.sh $4 1 $second $1 $MYTAG_$2 &
 sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --parent 10.10.1.1 --policy central --duration $1 --cap $2 > $3_$4_central_$2.csv
--- run_hierarchical.sh
+++ run_hierarchical.sh
@@ -1,6 +1,7 @@
-#Usage run_.sh [duration] [cap] [app1] [app2]
-first="${5:-high}"
-second="${6:-low}"
-bash exec_container.sh $3 0 $first $1 &
-bash exec_container.sh $4 1 $second $1 &
+#Usage run_.sh [duration] [cap] [app1] [app2] [mytag] [load1] [load2]
+MYTAG="${5:-fair}"
+first="${6:-high}"
+second="${7:-low}"
+bash exec_container.sh $3 0 $first $1 $MYTAG_$2 &
+bash exec_container.sh $4 1 $second $1 $MYTAG_$2 &
 sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --parent 10.10.1.1 --policy ml --duration $1 --cap $2 > $3_$4_hierarchical_$2.csv
