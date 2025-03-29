14ff97da2e5f8e61ff3054538dcb005a9b5c4f0a
Hyoungwook Nam <hwnam831@gmail.com>
2025-03-28 20:27:36 -0500

    Exp script fix

--- centralizedexp2.sh
+++ centralizedexp2.sh
@@ -5,8 +5,8 @@ DURATION=$(($3+120))
 #./WorkloadInvoker -c warmup.json & sleep 70;
 cd /mydata/workspace/jrapl;
 mkdir -p results;
-ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" llama-3.1-8b stable-diffusion config2_"$1"_central" &
-ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" stable-diffusion cnn-serving config2_"$1"_central" &
-ssh hwnam831@ow4 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" cnn-serving vits-ljs config2_"$1"_central" &
-ssh hwnam831@ow5 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" vits-ljs llama-3.1-8b config2_"$1"_central" &
+ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" llama-3.1-8b stable-diffusion config2_"$1"_central" 2> /dev/null &
+ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" stable-diffusion cnn-serving config2_"$1"_central" 2> /dev/null &
+ssh hwnam831@ow4 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" cnn-serving vits-ljs config2_"$1"_central" 2> /dev/null &
+ssh hwnam831@ow5 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" vits-ljs llama-3.1-8b config2_"$1"_central" 2> /dev/null &
 python3 ClusterController.py --policy $1 --limit $TOTALCAP --duration $DURATION > results/config2_centralized_$1_$2.csv;
--- centralizedexp3.sh
+++ centralizedexp3.sh
@@ -5,8 +5,8 @@ DURATION=$(($3+120))
 #./WorkloadInvoker -c warmup.json & sleep 70;
 cd /mydata/workspace/jrapl;
 mkdir -p results;
-ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" llama-3.1-8b stable-diffusion config3_"$1"_central high high" &
-ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" stable-diffusion cnn-serving config3_"$1"_central low low" &
-ssh hwnam831@ow4 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" cnn-serving vits-ljs config3_"$1"_central high high" &
-ssh hwnam831@ow5 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" vits-ljs llama-3.1-8b config3_"$1"_central low low" &
+ssh hwnam831@ow2 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" llama-3.1-8b llama-3.1-8b config3_"$1"_central" 2> /dev/null &
+ssh hwnam831@ow3 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" stable-diffusion stable-diffusion config3_"$1"_central" 2> /dev/null &
+ssh hwnam831@ow4 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" cnn-serving cnn-serving config3_"$1"_central" 2> /dev/null &
+ssh hwnam831@ow5 "cd /mydata/workspace/jrapl; bash run_centralized.sh "$DURATION" "$2" vits-ljs vits-ljs config3_"$1"_central" 2> /dev/null &
 python3 ClusterController.py --policy $1 --limit $TOTALCAP --duration $DURATION > results/config3_centralized_$1_$2.csv;
