a9785bca04b7fed328b6761d58721d2b71116f4b
Hyoungwook Nam <hwnam831@gmail.com>
2025-04-14 14:37:08 -0500

    big cluster and different seed ready

--- exec_container.sh
+++ exec_container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#usage: bash exec_container.sh [containername] [socket] [workloadlevel] [duration] [tag]
+#usage: bash exec_container.sh [containername] [socket] [workloadlevel] [duration] [tag] [seed]
 MYTAG="${5:-test}"
-sudo docker exec $1_$2 python run_model.py --workload $3 --duration $4 --idle 0.5 --tag $MYTAG
+MYSEED="${6:-1}"
+sudo docker exec $1_$2 python run_model.py --workload $3 --duration $4 --idle 0.5 --tag $MYTAG --config $MYSEED
--- run_centralized.sh
+++ run_centralized.sh
@@ -1,7 +1,8 @@
-#Usage run_.sh [duration] [cap] [app1] [app2] [tag] [load1] [load2]
+#Usage run_.sh [duration] [cap] [app1] [app2] [tag] [load1] [load2] [seed]
 MYTAG="${5:-fair}"
 first="${6:-high}"
 second="${7:-low}"
-bash exec_container.sh $3 0 $first $1 $MYTAG"_"$2 &
-bash exec_container.sh $4 1 $second $1 $MYTAG"_"$2 &
+MYSEED="${8:-1}"
+bash exec_container.sh $3 0 $first $1 $MYTAG"_"$2 $MYSEED &
+bash exec_container.sh $4 1 $second $1 $MYTAG"_"$2 $MYSEED &
 sudo java -cp $PWD":"$PWD"/argparse4j-0.9.0.jar" LocalController --parent 10.10.1.1 --policy central --duration $1 --cap $2 > $3_$4_central_$2.csv
