90030f35fa25c38d81593a9b06c1653cb8a69a67
Hyoungwook Nam <hwnam831@gmail.com>
2025-10-07 01:41:54 -0600

    clustercontroller fine tuning

--- collectlatencies.sh
+++ collectlatencies.sh
@@ -1,5 +1,5 @@
 mkdir -p latencies
-scp hwnam831@ow2:/mydata/workspace/jrapl/config\*.csv latencies/
-scp hwnam831@ow3:/mydata/workspace/jrapl/config\*.csv latencies/
-scp hwnam831@ow4:/mydata/workspace/jrapl/config\*.csv latencies/
-scp hwnam831@ow5:/mydata/workspace/jrapl/config\*.csv latencies/
+scp hwnam831@ow2:/mydata/workspace/jrapl/central\*.csv latencies/
+scp hwnam831@ow3:/mydata/workspace/jrapl/central\*.csv latencies/
+scp hwnam831@ow4:/mydata/workspace/jrapl/central\*.csv latencies/
+scp hwnam831@ow5:/mydata/workspace/jrapl/central\*.csv latencies/
