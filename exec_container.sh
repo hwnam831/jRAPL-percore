456f081cc6c080ff2da27698389203263d48d683
Hyoungwook Nam <hwnam831@gmail.com>
2025-10-08 02:56:32 -0600

    amx-run tuning done

--- exec_container.sh
+++ exec_container.sh
@@ -2,4 +2,4 @@
 #usage: bash exec_container.sh [containername] [socket] [workloadlevel] [duration] [tag] [seed]
 MYTAG="${4:-test}"
 MYSEED="${5:-1}"
-sudo docker exec $1 python run_model.py --workload $2 --duration $3 --idle 0.5 --tag $MYTAG --config $MYSEED
+sudo docker exec $1 python amx_run.py --workload $2 --duration $3 --idle 0.5 --tag $MYTAG --config $MYSEED
